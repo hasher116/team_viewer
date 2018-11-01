cb63f8d81aaf99fde89ff8d1920f0921a790acb8
hasher116 <44617755+hasher116@users.noreply.github.com>
2018-11-01 11:48:06 +0300

    Update widget.cpp

--- screen/widget.cpp
+++ screen/widget.cpp
@@ -58,9 +58,40 @@ void Widget::slotViewDestroyed(QObject *object)
 
 void Widget::serialize(QPixmap pixmap)
 {
-    QByteArray bytes;
-    QBuffer buffer(&bytes);
-    buffer.open(QIODevice::WriteOnly);
-    pixmap.save(&buffer, "PNG"); // writes pixmap into bytes in PNG format
-    qDebug() << bytes.length();
+    //Серверная часть
+    
+    QImage image(pixmap.toImage());
+    QByteArray ba;
+    QDataStream ds(&ba, QIODevice::WriteOnly);
+    
+        ds << (int)image.format();
+        ds << image.byteCount();
+        ds << image.width();
+        ds << image.height();
+        ds.writeRawData((char*)image.bits(), image.byteCount());
+        qDebug() << ba.length();
+        
+    //Клиентская часть
+
+    QByteArray ba1 = ba; //ПОЛУЧАЕШЬ ПО СОКЕТУ
+    QDataStream ds1(&ba1, QIODevice::ReadOnly);
+        
+            int format = - 1;
+            int bytesCount = -1;
+            int width = 0;
+            int height = 0;
+
+            ds1 >> format;
+            ds1 >> bytesCount;
+            ds1 >> width;
+            ds1 >> height;
+            uchar* bytes = new uchar[bytesCount];
+            ds1.readRawData((char*)bytes, bytesCount);
+
+            QImage* img = new QImage(bytes, width, height, (QImage::Format)format);
+            screenShot = QPixmap::fromImage(*img,Qt::AutoColor);
+            view2 = new QPixmapView(screenShot);
+            connect(view2,SIGNAL(destroyed(QObject*)),this,SLOT(slotViewDestroyed(QObject*)));
+            view2->show();
+
 }
